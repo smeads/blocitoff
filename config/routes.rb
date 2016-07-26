857d0d83ba998bfe24f5f39f08edacfcf69b99e1
Steven Meads <sbmeads@gmail.com>
2016-07-26 15:09:56 -0700

    User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, :only => [:show]
 
+  get 'users/show'
   get 'home/index'
 
-  root to: 'home#index'
+  root to: 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
