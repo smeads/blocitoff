4377a21bbfd4bf3f1539269c3db05f40d531e3ea
Steven Meads <sbmeads@gmail.com>
2016-08-31 09:07:19 -0700

    Changed application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ var blocmetrics = {};
 blocmetrics.report = function(eventName){
   var event = {event: { name: eventName }};
   var request = new XMLHttpRequest();
-  request.open("POST", "http://localhost:3000/api/events", true);
+  request.open("POST", "https://still-reaches-71761.herokuapp.com/api/events", true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(event));
   };
